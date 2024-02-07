ab29b1da6c83ff347a0c05e143627e6e32f84e0d
Muhammad Mahfud <laililmahvut@gmail.com>
2024-02-07 13:11:09 +0700

    first commit

---
+++ image-uploader.js
@@ -0,0 +1,285 @@
+const deleteButtonSvg = `<svg
+width="25"
+height="25"
+viewBox="0 0 20 20"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+>
+<path
+     d="M9.99984 1.6665C5.40817 1.6665 1.6665 5.40817 1.6665 9.99984C1.6665 14.5915 5.40817 18.3332 9.99984 18.3332C14.5915 18.3332 18.3332 14.5915 18.3332 9.99984C18.3332 5.40817 14.5915 1.6665 9.99984 1.6665ZM12.7998 11.9165C13.0415 12.1582 13.0415 12.5582 12.7998 12.7998C12.6748 12.9248 12.5165 12.9832 12.3582 12.9832C12.1998 12.9832 12.0415 12.9248 11.9165 12.7998L9.99984 10.8832L8.08317 12.7998C7.95817 12.9248 7.79984 12.9832 7.6415 12.9832C7.48317 12.9832 7.32484 12.9248 7.19984 12.7998C6.95817 12.5582 6.95817 12.1582 7.19984 11.9165L9.1165 9.99984L7.19984 8.08317C6.95817 7.8415 6.95817 7.4415 7.19984 7.19984C7.4415 6.95817 7.8415 6.95817 8.08317 7.19984L9.99984 9.1165L11.9165 7.19984C12.1582 6.95817 12.5582 6.95817 12.7998 7.19984C13.0415 7.4415 13.0415 7.8415 12.7998 8.08317L10.8832 9.99984L12.7998 11.9165Z"
+     fill="#E61C24"
+/>
+</svg>`
+
+const imageIconSvg = `<svg
+width="28"
+height="28"
+viewBox="0 0 28 28"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+class="mb-2"
+>
+<path
+     d="M25.6667 16.2167V18.8883C25.6667 23.135 23.135 25.6667 18.8884 25.6667H9.11169C6.13669 25.6667 3.99003 24.4183 2.98669 22.2017L3.11503 22.1083L8.85503 18.2583C9.78836 17.6283 11.1067 17.6983 11.935 18.4217L12.3317 18.7483C13.2417 19.53 14.7117 19.53 15.6217 18.7483L20.475 14.5833C21.385 13.8017 22.855 13.8017 23.765 14.5833L25.6667 16.2167Z"
+     fill="#667687"
+></path>
+<path
+     opacity="0.4"
+     d="M24.465 9.3335H21.035C19.5534 9.3335 18.6667 8.44683 18.6667 6.96516V3.53516C18.6667 3.0685 18.76 2.67183 18.9234 2.3335C18.9117 2.3335 18.9 2.3335 18.8884 2.3335H9.11171C4.86504 2.3335 2.33337 4.86516 2.33337 9.11183V18.8885C2.33337 20.1602 2.55504 21.2685 2.98671 22.2018L3.11504 22.1085L8.85504 18.2585C9.78837 17.6285 11.1067 17.6985 11.935 18.4218L12.3317 18.7485C13.2417 19.5302 14.7117 19.5302 15.6217 18.7485L20.475 14.5835C21.385 13.8018 22.855 13.8018 23.765 14.5835L25.6667 16.2168V9.11183C25.6667 9.10016 25.6667 9.0885 25.6667 9.07683C25.3284 9.24016 24.9317 9.3335 24.465 9.3335Z"
+     fill="#667687"
+></path>
+<path
+     d="M10.5001 12.11C12.0336 12.11 13.2767 10.8668 13.2767 9.33331C13.2767 7.7998 12.0336 6.55664 10.5001 6.55664C8.96654 6.55664 7.72339 7.7998 7.72339 9.33331C7.72339 10.8668 8.96654 12.11 10.5001 12.11Z"
+     fill="#667687"
+></path>
+<path
+     d="M24.465 1.1665H21.035C19.5533 1.1665 18.6666 2.05317 18.6666 3.53484V6.96484C18.6666 8.4465 19.5533 9.33317 21.035 9.33317H24.465C25.9466 9.33317 26.8333 8.4465 26.8333 6.96484V3.53484C26.8333 2.05317 25.9466 1.1665 24.465 1.1665ZM24.7216 5.02817C24.5816 5.16817 24.395 5.23817 24.2083 5.23817C24.0216 5.23817 23.835 5.16817 23.695 5.02817L23.485 4.81817V7.4315C23.485 7.83984 23.1583 8.1665 22.75 8.1665C22.3416 8.1665 22.015 7.83984 22.015 7.4315V4.81817L21.805 5.02817C21.525 5.30817 21.0583 5.30817 20.7783 5.02817C20.4983 4.74817 20.4983 4.2815 20.7783 4.0015L22.2366 2.54317C22.295 2.48484 22.3766 2.43817 22.4583 2.40317C22.4816 2.3915 22.505 2.3915 22.5283 2.37984C22.5866 2.3565 22.645 2.34484 22.715 2.34484C22.7383 2.34484 22.7616 2.34484 22.785 2.34484C22.8666 2.34484 22.9366 2.3565 23.0183 2.3915C23.03 2.3915 23.03 2.3915 23.0416 2.3915C23.1233 2.4265 23.1933 2.47317 23.2516 2.5315C23.2633 2.54317 23.2633 2.54317 23.275 2.54317L24.7333 4.0015C25.0133 4.2815 25.0133 4.74817 24.7216 5.02817Z"
+     fill="#667687"
+></path>
+</svg>`
+
+
+class ImageUploader {
+     elementUploader;
+     config = {
+          multiple: false,
+          compress: true,
+          height: '150px',
+          width: '300px',
+          maxUpload: 5,
+          maxSize: 2,
+          name: 'images[]'
+     }
+
+     constructor(selector, config) {
+          this.config = {
+               ...this.config,
+               ...config
+          }
+          this.elementUploader = document.querySelector(selector);
+          this.install()
+     }
+
+     install() {
+          var htmlButton = this.htmlAddMoreButton()
+          var htmlImageArea = this.htmlImageArea()
+          var html = `<div class="image-uploader-area">
+                         ${htmlButton}
+                         ${htmlImageArea}
+                    </div>`
+          this.elementUploader.innerHTML = html
+          this.listener()
+     }
+     listener() {
+          this.elementUploader.querySelectorAll('.image-block-content').forEach((uploadArea) => {
+               uploadArea.style.height = this.config.height;
+               uploadArea.style.width = this.config.width;
+               const inputFile = uploadArea.closest('.block-group-image').querySelector('input[type="file"]')
+               uploadArea.addEventListener("dragenter", (e) => {
+                    this.hadleDragDrop(e, inputFile, uploadArea)
+               }, false)
+               uploadArea.addEventListener("dragleave", (e) => {
+                    this.hadleDragDrop(e, inputFile, uploadArea)
+               }, false)
+               uploadArea.addEventListener("dragover", (e) => {
+                    this.hadleDragDrop(e, inputFile, uploadArea)
+               }, false)
+               uploadArea.addEventListener("drop", (e) => {
+                    this.hadleDragDrop(e, inputFile, uploadArea)
+               }, false)
+               uploadArea.addEventListener("click", (e) => {
+                    inputFile.click()
+                    e.preventDefault()
+               })
+               inputFile.addEventListener("change", (e) => {
+                    const files = e.target.files
+                    if (files) {
+                         this.fileListener(files, inputFile, uploadArea)
+                    }
+               })
+               this.listenerDeleteGroupImage(uploadArea)
+          })
+          if(this.config.multiple){
+               this.elementUploader.querySelector('.add-more-image').addEventListener('click',()=>{
+                    this.createNewImageArea()
+               })
+          }
+     }
+
+     createNewImageArea(){
+          const currentTotalImage =  this.elementUploader.querySelectorAll('.block-group-image').length + 1
+          if(currentTotalImage<=this.config.maxUpload){
+               var area = this.elementUploader.querySelector('.image-uploader-area')
+               var htmlImageArea = this.htmlImageArea()
+               var html = area.innerHTML + htmlImageArea;
+               area.innerHTML = ''
+               area.innerHTML = html
+               // area.insertAdjacentHTML('beforeend',htmlImageArea)
+               this.listener()
+               if(currentTotalImage==this.config.maxUpload){
+                    this.elementUploader.querySelector('.add-more-image').setAttribute('disabled',true)    
+               }
+          }
+     }
+
+     hadleDragDrop(e, inputFile, uploadArea) {
+          e.preventDefault()
+          e.stopPropagation()
+
+          if (e.dataTransfer && e.dataTransfer.files) {
+               const files = e.dataTransfer.files;
+               if (files) {
+                    inputFile.files = e.dataTransfer.files
+                    this.fileListener(files, inputFile, uploadArea)
+               }
+          }
+     }
+
+     fileListener(files, inputFile, uploadArea) {
+          const file = files[0]
+          if (file) {
+               const acceptedFileMimes = ["image/png", "image/jpeg", "image/jpg", "image/webp", "image/gif", "image/svg+xml"];
+               if (!acceptedFileMimes.includes(file.type)) {
+                    alert("Image format is not valid")
+                    inputFile.value = null
+                    inputFile.files = null
+                    return;
+               }
+               if (file.size > this.config.maxSize * 1024 * 1024) {
+                    alert(`Image size can't more then ${this.config.maxSize}mb`)
+                    return
+               }
+               if (this.config.compress) {
+                    this.compressImage(file, 2160, 2160, ".png", 0.8,
+                         (compressed) => {
+                              inputFile.file = compressed
+                         }
+                    )
+               }
+               this.createBlobBackground(file, uploadArea, inputFile)
+          }
+
+     }
+
+     listenerDeleteImage(uploadArea, inputFile, img) {
+          const button = uploadArea.closest('.block-group').querySelector('button')
+          button.style.display = "block"
+          button.addEventListener('click', (e) => {
+               img.setAttribute('src', '')
+               img.style.display = "none"
+               button.style.display = "none"
+               inputFile.value = ""
+               e.preventDefault()
+          })
+     }
+
+     listenerDeleteGroupImage(uploadArea){
+          if(this.config.multiple){
+               const _Self = this
+               uploadArea.closest('.block-group').querySelector('button').addEventListener('click',(e)=>{
+                    e.target.closest('.block-group-image').remove()
+                    _Self.elementUploader.querySelector('.add-more-image').removeAttribute('disabled')    
+                    // _Self.listener()
+               })
+          }
+     }
+
+     createBlobBackground(file, uploadArea, inputFile) {
+          const img = uploadArea.querySelector('img')
+          img.setAttribute('src', URL.createObjectURL(file))
+          img.style.display = "block"
+          this.listenerDeleteImage(uploadArea, inputFile, img)
+     }
+
+
+     compressImage(file, maxWidth, maxHeight, outputFormat, quality, callback) {
+          const _Self = this
+          const reader = new FileReader();
+          reader.onload = function (event) {
+               const img = new Image();
+               img.src = URL.createObjectURL(file)
+
+               img.onload = function () {
+                    let width = img.width;
+                    let height = img.height;
+                    if (width > maxWidth) {
+                         height *= maxWidth / width;
+                         width = maxWidth;
+                    }
+                    if (height > maxHeight) {
+                         width *= maxHeight / height;
+                         height = maxHeight;
+                    }
+                    const canvas = document.createElement('canvas');
+                    canvas.width = width;
+                    canvas.height = height;
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(img, 0, 0, width, height);
+                    const compressedImageData = canvas.toDataURL(`image/${outputFormat}`, quality);
+                    callback(_Self.dataURLtoBlobToFile(compressedImageData, file.name));
+               };
+               img.src = event.target.result;
+          };
+          reader.readAsDataURL(file);
+     }
+
+     dataURLtoBlobToFile(dataurl, name) {
+          var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+               bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+          while (n--) {
+               u8arr[n] = bstr.charCodeAt(n);
+          }
+          var blob = new Blob([u8arr], { type: mime });
+          return new File([blob], name);
+     }
+
+
+     
+     htmlImageArea(){
+          var displayButton = this.config.multiple ? 'block' : 'none'
+          return ` <div class="block-group block-group-image">
+                    <div class="image-block-content" draggable="true">
+                         <div class="empty">
+                              ${imageIconSvg}
+                              <div><b>Drag</b> or <b>click</b> to upload image</div>
+                         </div>
+                         <img src="" alt="" />
+                    </div>
+                    <input
+                         type="file"
+                         name="${this.config.name}"
+                         required
+                         accept="image/png,image/gif,image/jpeg,image/svg+xml,image/webp"
+                    />
+                    <button type="button" style="display:${displayButton}">
+                         ${deleteButtonSvg}
+                    </button>
+               </div>`
+     }
+     htmlAddMoreButton() {
+          var html = ''
+          if (this.config.multiple) {
+               html += `<button type="button" class="block-group add-more-image" style="height:${this.config.height}">
+                         <svg
+                              width="45"
+                              height="44"
+                              viewBox="0 0 45 44"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                         >
+                              <path
+                                   d="M33.8999 23.375H11.8999C11.1482 23.375 10.5249 22.7517 10.5249 22C10.5249 21.2483 11.1482 20.625 11.8999 20.625H33.8999C34.6516 20.625 35.2749 21.2483 35.2749 22C35.2749 22.7517 34.6516 23.375 33.8999 23.375Z"
+                                   fill="#075487"
+                              />
+                              <path
+                                   d="M22.8999 34.375C22.1482 34.375 21.5249 33.7517 21.5249 33V11C21.5249 10.2483 22.1482 9.625 22.8999 9.625C23.6516 9.625 24.2749 10.2483 24.2749 11V33C24.2749 33.7517 23.6516 34.375 22.8999 34.375Z"
+                                   fill="#075487"
+                              />
+                         </svg>
+
+                         Add Image
+                    </button>`
+          }
+          return html
+     }
+}
